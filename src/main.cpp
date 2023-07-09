5263d0be77ab0ea4d182dcc36e4a4a5d80032311
JamesBremner <james@ravenspoint.com>
2023-07-09 11:48:03 -0400

    Basic algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,128 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <wex.h>
-#include "cStarterGUI.h"
 
-class cGUI : public cStarterGUI
+#define HOURS_PER_DAY 24
+#define TOTAL_AVAILABLE_HOURS 30 * HOURS_PER_DAY
+
+class cTask
 {
 public:
-    cGUI()
-        : cStarterGUI(
-              "Starter",
-              {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
-    {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
+    std::string name;
+    int duration;
+    int wStart;
+    int wEnd;
+    int actualStart;
 
-        show();
-        run();
-    }
-
-private:
-    wex::label &lb;
+    std::string text();
 };
 
-main()
+class cSchedule
 {
-    cGUI theGUI;
+public:
+    std::vector<bool> vBusyHour;
+    std::vector<cTask> vTask;
+
+    cSchedule()
+    {
+        vBusyHour.resize(TOTAL_AVAILABLE_HOURS, false);
+    }
+    void add(cTask &t);
+};
+
+
+
+
+
+std::string cTask::text()
+{
+    std::stringstream ss;
+    if (actualStart < 0)
+    {
+        ss << "task " << name << " cannot be scheduled\n";
+    }
+    else
+    {
+        int day = actualStart / HOURS_PER_DAY;
+        int hour = actualStart - day * HOURS_PER_DAY;
+        ss << "task " << name
+           << " scheduled at " << hour << " on day " << day;
+    }
+    return ss.str();
+}
+
+void cSchedule::add(cTask &t)
+{
+    t.actualStart = -1;
+
+    // loop over window
+    for (
+        int delay = 0;
+        delay < t.wEnd - t.wStart - t.duration;
+        delay++)
+    {
+        // check that no overlap with other task
+        bool okBusy = true;
+        int StartHour = t.wStart * HOURS_PER_DAY + delay;
+        int EndHour = StartHour + t.duration;
+        for (int hour = StartHour;
+             hour < EndHour;
+             hour++)
+        {
+            if (vBusyHour[hour])
+            {
+                // overlap
+                okBusy = false;
+                break;
+            }
+        }
+        if (okBusy)
+        {
+            // add task to schedule
+            for (int hour = StartHour; hour < EndHour; hour++)
+                vBusyHour[hour] = true;
+            t.actualStart = StartHour;
+            break;
+        }
+    }
+
+    vTask.push_back( t );
+
+    for (auto &t : vTask)
+        std::cout << t.text() << "\n";
+}
+
+
+void readfile(
+    cSchedule& S,
+    const std::string &fname)
+{
+    std::ifstream ifs(fname);
+    if (!ifs.is_open())
+        throw std::runtime_error(
+            "Cannot open input file");
+    std::string type;
+    cTask task;
+    ifs >> type >> task.name >> task.duration >> task.wStart >> task.wEnd;
+    while (ifs.good())
+    {
+        std::cout << "\nAdding " 
+            << task.name << " " << task.duration << " " << task.wStart << " " << task.wEnd << "\n";
+
+        S.add(task);
+
+        ifs >> type >> task.name >> task.duration >> task.wStart >> task.wEnd;
+    }
+}
+
+main(int argc, char *argv[])
+{
+    if (argc != 2)
+        throw std::runtime_error(
+            "Cannot open input file");
+
+    cSchedule S;
+    readfile(S,argv[1]);
+
     return 0;
 }
